f924b50d1af03815903bd6e9213237071785121b
matt shilling <matthewds@sky.com>
2024-07-01 15:05:18 +0100

    I don't understand really :(

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "ask", to: "questions#ask", as: :ask
+
+  get "answer", to: "questions#answer", as: :answer
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
