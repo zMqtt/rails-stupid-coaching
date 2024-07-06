9001abc6ea28be773997610ec27b5d009131f62c
matt shilling <matthewds@sky.com>
2024-07-06 16:06:21 +0100

    final version of stupid coaching with rails

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,10 +4,12 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == "I am going to work"
+    if @question.blank?
+      @answer = "I CAN'T HEAR YOU!"
+    elsif @question =~ /"i am going to work"/i
       @answer = "Great!"
     elsif @question.ends_with?("?")
-     @answer = "Silly question, get dressed and go to work!"
+      @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
